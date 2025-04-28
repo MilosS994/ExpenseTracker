96b2282867191188db92bb0b8b4bcc809ace7e1e
Milos Srejic <srejic.milos94@gmail.com>
2025-04-28 19:18:56 +0200

    Implement profile photo selector component for letting user choose profile image

---
+++ client/src/components/ProfilePhotoSelector.jsx
@@ -0,0 +1,71 @@
+// IMPORTS
+// React hooks
+import { useRef, useState } from "react";
+// React icons
+import { LuUser, LuUpload, LuTrash } from "react-icons/lu";
+
+const ProfilePhotoSelector = ({ image, setImage }) => {
+  const inputRef = useRef(null);
+  const [preview, setPreview] = useState(null);
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImage(file); // Set the image file in the parent component
+
+      const preview = URL.createObjectURL(file); // Create a preview URL for the image
+      setPreview(preview);
+    }
+  };
+
+  const handleRemoveImage = () => {
+    setImage(null); // Remove the image file in the parent component
+    setPreview(null); // Clear the preview URL
+  };
+
+  const onChooseImage = () => {
+    inputRef.current.click();
+  };
+  return (
+    <div className="flex justify-start mt-2">
+      <input
+        type="file"
+        accept="image/*"
+        ref={inputRef}
+        onChange={handleImageChange}
+        className="hidden"
+      />
+
+      {!image ? (
+        <div className="w-20 h-20 flex items-center justify-center bg-tertiary border-gray-200 border-1 rounded-full relative">
+          <LuUser className="text-4xl text-black" />
+
+          <button
+            type="button"
+            className="w-8 h-8 flex items-center justify-center bg-secondary text-white rounded-full absolute -bottom-1 -right-2 cursor-pointer hover:bg-primary transition duration-200"
+            onClick={onChooseImage}
+          >
+            <LuUpload />
+          </button>
+        </div>
+      ) : (
+        <div className="relative">
+          <img
+            src={preview}
+            alt="Profile photo"
+            className="w-20 h-20 rounded-full object-cover"
+          />
+          <button
+            type="button"
+            className="w-8 h-8 flex items-center justify-center bg-red-300 text-white rounded-full absolute -bottom-1 -right-2 cursor-pointer hover:bg-red-400 transition duration-200"
+            onClick={handleRemoveImage}
+          >
+            <LuTrash />
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ProfilePhotoSelector;
